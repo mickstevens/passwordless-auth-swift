d7bcaff19b73c59e2adf48a11891c8eafbee3643
Ricky Robinett <ricky.robinett@gmail.com>
2015-03-03 14:49:22 -0500

    Initial Commit

---
+++ PasswordlessAuthPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordlessAuthPost
+//
+//  Created by Ricky Robinett on 3/3/15.
+//  Copyright (c) 2015 Ricky Robinett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
